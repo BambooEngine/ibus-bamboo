03692d583165712bb1fdbea0cbabe7a0edd01038
luongthanhlam <ltlam93@gmail.com>
2019-04-30 16:17:33 +0700

    Do not commit text if buffer is empty

--- src/ibus-bamboo/engine_utils.go
+++ src/ibus-bamboo/engine_utils.go
@@ -59,6 +59,8 @@ func GetIBusBambooEngine() func(conn *dbus.Conn, engineName string) dbus.ObjectP
 		keyPressHandler = engine.keyPressHandler
 
 		onMouseMove = func() {
+			engine.Lock()
+			defer engine.Unlock()
 			engine.ignorePreedit = false
 			x11ClipboardReset()
 			engine.resetFakeBackspace()
