da3f93586dab1c2a21c0069540d484c5027bb2cd
luongthanhlam <ltlam93@gmail.com>
2021-05-09 08:49:34 +0700

    Use org.gnome.Overview as wmClass instead of empty

--- src/ibus-bamboo/gnome_introspector.go
+++ src/ibus-bamboo/gnome_introspector.go
@@ -6,7 +6,7 @@ import (
 	"github.com/godbus/dbus"
 )
 
-func gnomeGetFocusWindowClass() ( string, error ) {
+func gnomeGetFocusWindowClass() (string, error) {
 	conn, err := dbus.SessionBus()
 	var s string
 	if err != nil {
@@ -23,30 +23,24 @@ func gnomeGetFocusWindowClass() ( string, error ) {
 	var ok bool
 	err = obj.Call("org.gnome.Shell.Eval", 0, js_code).Store(&ok, &s)
 	if !ok {
-		err = errors.New(s)
+		if isGnomeOverviewVisible(conn) {
+			return "org.gnome.Overview", nil
+		} else {
+			err = errors.New(s)
+		}
 	}
-	if (err != nil) {
+	if err != nil {
 		return "", err
 	}
 	return s, nil
 }
 
-func isGnomeOverviewVisible() ( bool ) {
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return false
-	}
-	defer func() {
-		if err = conn.Hello(); err == nil {
-			conn.Close()
-		}
-	}()
-
+func isGnomeOverviewVisible(conn *dbus.Conn) bool {
 	js_code := "Main.overview.visible"
 	obj := conn.Object("org.gnome.Shell", "/org/gnome/Shell")
 	var visible string
 	var ok bool
-	err = obj.Call("org.gnome.Shell.Eval", 0, js_code).Store(&ok, &visible)
+	err := obj.Call("org.gnome.Shell.Eval", 0, js_code).Store(&ok, &visible)
 	if !ok || err != nil {
 		return false
 	}
