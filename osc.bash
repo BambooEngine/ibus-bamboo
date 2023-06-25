986333ce7a7fbb562522fd6902a15b3c0de8c16c
Lâm <ltlam93@gmail.com>
2023-06-25 18:12:46 +0700

    Add gh actions instead of travis-ci

--- osc.bash
+++ osc.bash
@@ -1,35 +1,26 @@
 #!/bin/bash
-if [[ $TRAVIS_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-  echo "Release tag: $TRAVIS_TAG"
+if [[ $GH_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo "Release tag: $GH_TAG"
 else
+  echo "Release tag ($GH_TAG) is invalid, exiting"
   exit 0
 fi
+#set -euxo pipefail
 echo "[general]" >> ~/.oscrc
 echo "apiurl = https://api.opensuse.org" >> ~/.oscrc
 echo "[https://api.opensuse.org]" >> ~/.oscrc
 echo "user = $OSC_USER" >> ~/.oscrc
 echo "pass = $OSC_PASS" >> ~/.oscrc
+export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get update
-sudo apt-get install osc -y
-echo "osc install"
-
-mkdir ../build
-cd ../build
-echo "osc checkout"
-yes 2 | osc checkout $OSC_PATH
-cd $TRAVIS_BUILD_DIR
-rm -rf ../build/$OSC_PATH/*
-echo "osc build"
-make build src DESTDIR=../build/$OSC_PATH
-cd ../build/$OSC_PATH
+mkdir /build && pushd /build
+osc checkout $OSC_PATH
+popd
+rm -rf /build/$OSC_PATH/*
+make build src DESTDIR=/build/$OSC_PATH
+cd /build/$OSC_PATH
 osc add *.spec *.changes *.tar.gz
-echo "osc addremove"
 osc addremove
-echo "osc st"
 osc st
-echo "osc commit"
-echo "$TRAVIS_TAG"
-yes 2 | osc ci -m "$TRAVIS_TAG"
-echo "osc done"
+osc ci -m "$GH_TAG"
 exit 0
